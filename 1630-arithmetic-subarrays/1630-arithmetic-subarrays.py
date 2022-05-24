b96f47f68e55c196bfd0445a6a85dc742c9ac9e1
abi26anamo <91598495+abi26anamo@users.noreply.github.com>
2022-05-25 00:06:12 +0300

    Time: 216 ms (75.92%), Space: 14.2 MB (55.66%) - LeetHub

--- 1630-arithmetic-subarrays/1630-arithmetic-subarrays.py
+++ 1630-arithmetic-subarrays/1630-arithmetic-subarrays.py
@@ -5,7 +5,8 @@ class Solution:
             temp = sorted(nums[l[i]:r[i]+1])
             x = temp[1] - temp[0]
             output = False
-            for j in range(1, len(temp)):
+            for j in range(1, len(temp)): 
+                
                 if temp[j]-temp[j-1] != x:
                     output = True
                     break
