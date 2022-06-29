7920039c4d40eb54c63b911b9bb90e9ce4d262d2
abi26anamo <91598495+abi26anamo@users.noreply.github.com>
2022-06-29 23:29:45 +0300

    Time: 53 ms (60.68%), Space: 14.3 MB (7.12%) - LeetHub

---
+++ 486-predict-the-winner/486-predict-the-winner.py
@@ -0,0 +1,18 @@
+class Solution:
+	def PredictTheWinner(self, nums: List[int]) -> bool:
+		nums_len = len(nums)
+		memo = dict()
+
+		def get_scores(left, right):
+			if (left, right) in memo: 
+				return memo[(left, right)]
+			if left==right:
+				memo[(left, right)] = nums[left]
+				return memo[(left, right)] 
+			elif left<right: 
+				memo[(left, right)] = max(nums[left] - get_scores(left+1,right), nums[right] -                         get_scores(left,right-1))
+				return memo[(left, right)]
+
+		diff = get_scores(0, nums_len-1)
+		return True if diff>=0 else False
+        
