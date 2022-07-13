98c425819deb733b473e9d64a0d50cea39ec47fb
abi26anamo <91598495+abi26anamo@users.noreply.github.com>
2022-07-13 11:42:14 +0300

    Time: 1637 ms (9.17%), Space: 46.6 MB (47.86%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.py
+++ 234-palindrome-linked-list/234-palindrome-linked-list.py
@@ -5,31 +5,35 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        curr = head
-        N=0
-        while curr:
-            N+=1
-            curr =curr.next
+#         curr = head
+#         N=0
+#         while curr:
+#             N+=1
+#             curr =curr.next
             
-        mid = N//2
-        i=0
-        def reverse(head):
-            ans= None
-            while head:
-                nxt = head.next
-                head.next = ans
-                ans =head
-                head = nxt
-            return ans
-        first = second = head
-        while i <mid:
-            second = second.next
-            i+=1
-        second =reverse(second)
-        while first and second:
-            if first.val != second.val:return False
-            first = first.next
-            second =second.next
-        return True
-    
-                
+#         mid = N//2
+#         i=0
+#         def reverse(head):
+#             ans= None
+#             while head:
+#                 nxt = head.next
+#                 head.next = ans
+#                 ans =head
+#                 head = nxt
+#             return ans
+#         first = second = head
+#         while i <mid:
+#             second = second.next
+#             i+=1
+#         second =reverse(second)
+#         while first and second:
+#             if first.val != second.val:return False
+#             first = first.next
+#             second =second.next
+#         return True
+        stack = []
+        while head:
+            stack.append(head.val)
+            head = head.next
+        return stack == stack[::-1]
+
