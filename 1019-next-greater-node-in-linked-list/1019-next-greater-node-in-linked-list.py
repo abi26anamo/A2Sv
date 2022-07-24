d5417d25c26d42944be1ed5e1fb0c9555ccd6f1a
abi26anamo <91598495+abi26anamo@users.noreply.github.com>
2022-07-24 23:04:55 +0300

    Time: 480 ms (46.65%), Space: 19.2 MB (20.34%) - LeetHub

--- 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.py
+++ 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.py
@@ -9,22 +9,36 @@ class Solution:
                 :type head: ListNode
                 :rtype: List[int]
                 """
-                prev = None
-                while head:
-                    temp = head.next
-                    head.next = prev
-                    prev = head
-                    head = temp
-                head = prev
-                res = []
-                st = []
-                while head:
-                    while st and st[-1] <= head.val:
-                        st.pop()
-                    if st:
-                        res.append(st[-1])
-                    else:
-                        res.append(0)
-                    st.append(head.val)
-                    head = head.next
-                return res[::-1]
+                arr =[]
+                current = head
+                while current:
+                    arr.append(current.val)
+                    current = current.next
+                ans =[0]*len(arr)
+                stack =[]
+                for i in reversed(range(len(arr))):
+                    while len(stack)>0 and stack[-1] <= arr[i]:
+                        stack.pop()
+                    if len(stack)!=0:
+                        ans[i] = stack[-1]
+                    stack.append(arr[i])
+                return ans
+                # prev = None
+                # while head:
+                #     temp = head.next
+                #     head.next = prev
+                #     prev = head
+                #     head = temp
+                # head = prev
+                # res = []
+                # st = []
+                # while head:
+                #     while st and st[-1] <= head.val:
+                #         st.pop()
+                #     if st:
+                #         res.append(st[-1])
+                #     else:
+                #         res.append(0)
+                #     st.append(head.val)
+                #     head = head.next
+                # return res[::-1]
