0a42d10ed4d79014eb8fdfd7e299def8e5df597b
abi26anamo <91598495+abi26anamo@users.noreply.github.com>
2022-08-03 22:39:57 -0700

    Time: 59 ms (24.84%), Space: 13.9 MB (14.03%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.py
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -12,17 +12,3 @@ class Solution:
         else:
             return -heap[0]
         
-#         # invert the value of the keys and use heapq 
-#         # so that we can get max-heap
-#         heap = [-x for x in stones]
-#         heapify(heap)
-        
-#         # smash stones
-#         while len(heap) > 1:                       
-#             x, y = heappop(heap), heappop(heap)     # get the heaviest two stones
-#             if x != y:
-#                 heappush(heap, x - y)               # store the new weight back to the heap
-                
-#         # check if there is remaining stone 
-#         if len(heap) == 0: return 0
-#         else: return -heap[0] 
