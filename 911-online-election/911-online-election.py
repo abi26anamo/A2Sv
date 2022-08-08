b356184daf90a1cafd446b3ab7e9c32bff347d88
abi26anamo <91598495+abi26anamo@users.noreply.github.com>
2022-08-08 03:40:30 -0700

    Time: 1418 ms (27.03%), Space: 19.7 MB (96.77%) - LeetHub

---
+++ 911-online-election/911-online-election.py
@@ -0,0 +1,33 @@
+class TopVotedCandidate:
+
+    def __init__(self, persons: List[int], times: List[int]):
+        count = Counter()
+        max_vote = 0
+        self.lead =[]
+        for i,c in enumerate(times):
+            count[persons[i]] +=1
+            if count[persons[i]] >=max_vote:
+                max_vote = count[persons[i]]
+                self.lead.append([c,persons[i]])
+                
+        
+
+    def q(self, t: int) -> int:
+        l=0
+        r =len(self.lead)-1
+        while l <=r:
+            mid = (l+r)//2
+            if self.lead[mid][0]<=t:
+                res = mid
+                l = mid+1
+            else:
+                r = mid-1
+        return self.lead[res][1]
+                
+
+
+# Your TopVotedCandidate object will be instantiated and called as such:
+# obj = TopVotedCandidate(persons, times)
+# param_1 = obj.q(t)
+
+
