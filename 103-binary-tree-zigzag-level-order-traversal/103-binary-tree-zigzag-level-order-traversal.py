7337d759c2a04128aac68dfb7525906bc8db4dfa
abi26anamo <91598495+abi26anamo@users.noreply.github.com>
2022-09-02 00:56:18 -0700

    Time: 44 ms (70.78%), Space: 14.1 MB (96.09%) - LeetHub

--- 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.py
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.py
@@ -6,23 +6,46 @@
 #         self.right = right
 class Solution:
     def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        res =[]
-        stack = [root]
-        dirc = 1
-        if not root:
-            return []
-        while stack:
-            temp = []
-            for x in range(len(stack)):
-                node = stack.pop(0)
-                temp.append(node.val)
-                if node.left !=None:
-                    stack.append(node.left)
-                if node.right!=None:
-                    stack.append(node.right)
-            if dirc % 2 ==0:
-                res.append(temp[::-1])
-            else:
-                res.append(temp[::1])
-            dirc+=1
+        if not root:return []
+        queue = deque([root])
+        output = []
+        while queue:
+            levelCount =len(queue)
+            ans = []
+            for i in range(levelCount):
+                node = queue.popleft()
+                ans.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            output.append(ans)
+            res =[]
+            for i in range(len(output)):
+                if i%2 !=0:
+                     res.append(output[i][::-1])
+                else:
+                    res.append(output[i])
         return res
+            
+                
+        # res =[]
+        # stack = [root]
+        # dirc = 1
+        # if not root:
+        #     return []
+        # while stack:
+        #     temp = []
+        #     for x in range(len(stack)):
+        #         node = stack.pop(0)
+        #         temp.append(node.val)
+        #         if node.left !=None:
+        #             stack.append(node.left)
+        #         if node.right!=None:
+        #             stack.append(node.right)
+        #     if dirc % 2 ==0:
+        #         res.append(temp[::-1])
+        #     else:
+        #         res.append(temp[::1])
+        #     dirc+=1
+        # return res
