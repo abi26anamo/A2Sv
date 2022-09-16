aa6ab3923639b75e80d3d928e05083e65bab23e2
abi26anamo <91598495+abi26anamo@users.noreply.github.com>
2022-09-16 06:30:21 -0700

    Time: 81 ms (56.50%), Space: 14.2 MB (14.97%) - LeetHub

---
+++ 10-regular-expression-matching/10-regular-expression-matching.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        dic = {}
+        def dfs(i,j):
+            if (i,j) in dic:
+                return dic[(i,j)]
+            if i>=len(s)and j>=len(p):
+                return True
+            if j>=len(p):
+                return False
+            match =i<len(s) and (s[i]==p[j] or p[j]=='.')
+            if (j+1)<len(p) and p[j+1] =='*':
+                dic[(i,j)] = (dfs(i,j+2) or (match and dfs(i+1,j)))
+                return dic[(i,j)]
+            if match:
+                dic[(i,j)] = dfs(i+1,j+1)
+                return dic[i,j]
+            dic[(i,j)] = False
+            return False
+        return dfs(0,0)
+                
+                
