f46a9f938e644a68b0d4d60dbb7e357d00811e60
abi26anamo <91598495+abi26anamo@users.noreply.github.com>
2022-12-20 18:11:41 +0300

    Create finddifference.py

---
+++ finddifference.py
@@ -0,0 +1,7 @@
+def findTheDifference(self, s: str, t: str) -> str:
+       s=Counter(s)
+       t=Counter(t)
+       for i,v in t.items():
+           if s[i]==v:
+               continue
+           return i
