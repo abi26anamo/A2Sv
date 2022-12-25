e2f3225658b32abf015b9303981067f33fb37843
abi26anamo <91598495+abi26anamo@users.noreply.github.com>
2022-12-25 11:40:03 +0300

    Time: 29 ms (98.34%), Space: 13.9 MB (31.63%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,12 +1,19 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
+        
         s = Counter(s)
         t = Counter(t)
         for i,v in t.items():
-            if s[i]==v:
+            if s[i] == v:
                 continue
             return i
-            
+          
+  
+                
+      
+       
+     
+       
    
             
         
