c0f4f810e9911763ef587339907f9cd28d341c24
abi26anamo <91598495+abi26anamo@users.noreply.github.com>
2022-12-25 15:35:54 +0300

    Time: 41 ms (79.25%), Space: 13.8 MB (88.11%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -7,6 +7,7 @@ class Solution:
         for i in range(n):
             
             for s in strs:
+                
                 if i == len(s) or s[i]!=strs[0][i]:
                     return res
                 
