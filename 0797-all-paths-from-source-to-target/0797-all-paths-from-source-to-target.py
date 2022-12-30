ab5902056883db68bd4a8a56edf74701c671d425
abi26anamo <91598495+abi26anamo@users.noreply.github.com>
2022-12-30 07:35:39 +0300

    Time: 105 ms (82.79%), Space: 15.8 MB (43.50%) - LeetHub

---
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
@@ -0,0 +1,12 @@
+class Solution:
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        res =[]
+        def dfs(cur,path):
+            if cur == len(graph)-1:
+                res.append(path)
+            else:
+                for i in graph[cur]:
+                    dfs(i,path+[i])
+        dfs(0,[0])
+        return res
+                
