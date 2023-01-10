5a3c7d9290f2c335381fb062bae46a47b88634b5
abi26anamo <91598495+abi26anamo@users.noreply.github.com>
2023-01-10 23:57:00 +0300

    Update multiplystrings.py

--- multiplystrings.py
+++ multiplystrings.py
@@ -1,4 +1,12 @@
-  def multiply(self, num1: str, num2: str) -> str:
-        num1 = int(num1)
-        num2 = int(num2)
-        return str(num1*num2)
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        
+        first_num =0
+        for i in num1:
+            first_num = first_num*10+(ord(i)-ord('0'))
+            
+        second_num =0
+        for i in num2:
+            second_num = second_num*10+(ord(i)-ord('0'))
+            
+        return str(first_num*second_num)
