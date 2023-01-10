3316669bb1afd1fc48e052620cebe8d1fe27990a
abi26anamo <91598495+abi26anamo@users.noreply.github.com>
2023-01-10 10:37:21 +0300

    Create queen_that_attack_king.py

---
+++ queen_that_attack_king.py
@@ -0,0 +1,24 @@
+class Solution:
+    def queensAttacktheKing(self, queens: List[List[int]], king: List[int]) -> List[List[int]]:
+        
+        queen_can_attack =[]
+        visited = []
+        
+        for x,y in queens:
+            visited.append((x,y))
+        
+        directions = {(-1,-1),(0,-1),(-1,0),(1,1),(1,0),(0,1),(1,-1),(-1,1)}
+        
+        for x_dir,y_dir in directions:
+            
+                x_king,y_king = king[0],king[1]
+
+                while 0<=x_king+x_dir<8 >y_king+y_dir>=0:
+                    x_king+=x_dir
+                    y_king+=y_dir
+
+                    if (x_king,y_king) in visited:
+                        queen_can_attack+=[[x_king,y_king]]
+                        break
+
+        return queen_can_attack
