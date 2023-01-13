e8f876736a320213c221f2382e7c2b81d8058c52
abi26anamo <91598495+abi26anamo@users.noreply.github.com>
2023-01-13 21:55:36 +0300

    Create apply_array_operation.py

---
+++ apply_array_operation.py
@@ -0,0 +1,14 @@
+class Solution:
+    def applyOperations(self, nums: List[int]) -> List[int]:
+        
+        for i in range(len(nums)-1):
+            if nums[i]==nums[i+1]:
+                nums[i] *=2
+                nums[i+1]=0
+        
+        for i in range(len(nums)):
+            if nums[i]==0:
+                nums.remove(nums[i])
+                nums.append(0)
+        return nums
+    
