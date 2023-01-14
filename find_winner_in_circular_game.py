9f3c2a14521a76ebe2d423bbe6d85cd7992937d8
abi26anamo <91598495+abi26anamo@users.noreply.github.com>
2023-01-14 13:15:29 +0300

    Create find_winner_in_circular_game.py

---
+++ find_winner_in_circular_game.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findTheWinner(self, n: int, k: int) -> int:
+        
+        friends =  [i for i in range(1,n+1)]
+        i=0
+        while len(friends)>1:
+            i = (i+k-1)%len(friends)
+            friends.pop(i)
+
+        return friends[0]
+        
