195fc7c139a83b7627fef8e308b387ca8b771de4
abi26anamo <91598495+abi26anamo@users.noreply.github.com>
2023-01-19 21:07:26 +0300

    Create how_many_numbers_less_than_current_number.py

---
+++ how_many_numbers_less_than_current_number.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def smallerNumbersThanCurrent(self, nums):
+        sortedNums = sorted(nums)
+        dic = {}
+        result = []
+	
+        for i in range(len(sortedNums)):
+            if sortedNums[i] not in dic:
+                dic[sortedNums[i]] = i
+                
+        for i in nums:
+            result.append(dic[i])
+        return result
