f97d6f6b919765f13273ef6b8290ddb5c99fb32c
abi26anamo <91598495+abi26anamo@users.noreply.github.com>
2023-01-28 13:03:47 +0300

    Create boats_to_hold_people.py

---
+++ boats_to_hold_people.py
@@ -0,0 +1,13 @@
+   people.sort()
+        boats = 0
+        right = len(people)-1
+        left = 0
+        while left<=right:
+            if people[right]+people[left]<=limit:
+                boats+=1
+                left+=1
+                right-=1
+            else:
+                boats+=1
+                right-=1
+        return boats
