5efe5d64a22b940d4c948e1f3c6e6283861a008d
abi26anamo <91598495+abi26anamo@users.noreply.github.com>
2023-01-28 13:51:29 +0300

    Update max_number_k_sum.py

--- max_number_k_sum.py
+++ max_number_k_sum.py
@@ -1,15 +1,18 @@
 class Solution:
    
     def maxOperations(self, nums: List[int], k: int) -> int:
-        count = Counter(nums)
-        oper =0
-        pair = set()
-        for x in count:
-            if x not in pair and (k-x) in count:
-                if (k-x)==x:
-                    oper+=count[x]//2
-                else:
-                    oper+=min(count[x],count[k-x])
-                    pair.add(x)
-                    pair.add(k-x)
-        return oper
+        nums.sort()
+        left = 0
+        right = len(nums)-1
+        operations = 0
+        while left<right:
+            curr = nums[left]+nums[right]
+            if curr ==k:
+                operations+=1
+                left+=1
+                right-=1
+            elif curr>k:
+                right-=1
+            else:
+                left+=1
+        return operations
