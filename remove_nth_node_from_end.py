678e0c7d7f32c167ca2e53fb8bf9b4f745153f42
abi26anamo <91598495+abi26anamo@users.noreply.github.com>
2023-02-17 22:21:59 +0300

    Create remove_nth_node_from_end.py

---
+++ remove_nth_node_from_end.py
@@ -0,0 +1,29 @@
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        # first = second = head
+        # for _ in range(n):
+        #     first = first.next
+        #     if not first:
+        #         return head.next
+        # while first.next:
+        #     first = first.next
+        #     second = second.next
+        # second.next = second.next.next
+        # return head
+        
+        temp = head
+        count = 0
+        while temp:
+            count+=1
+            temp = temp.next
+        count = count-n
+        temp = head
+        
+        if count ==0:
+            return head.next
+        while count >1:
+            count-=1
+            temp = temp.next
+        temp.next = temp.next.next
+        return head
+        
