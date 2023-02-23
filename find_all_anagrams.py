43ebe19786a2f43b291f84cdf0f2fcb555448864
abi26anamo <91598495+abi26anamo@users.noreply.github.com>
2023-02-23 15:37:58 +0300

    Create find_all_anagrams.py

---
+++ find_all_anagrams.py
@@ -0,0 +1,31 @@
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        p_count = Counter(p)
+        k = len(p)
+        letter_map = {}
+        result = []
+        left = 0
+        
+        for right, letter in enumerate(s):
+            # add to letter_map
+            if letter in p_count.keys():
+                if letter not in letter_map:
+                    letter_map[letter] = 1
+                else:
+                    letter_map[letter] += 1
+            else:
+                left = right + 1
+                letter_map = {}
+                
+            # check window
+            if right-left+1 == k:
+                if p_count == letter_map:
+                    result.append(left)
+                    
+                #move window
+                letter_map[s[left]] -= 1
+                if letter_map[s[left]] == 0:
+                    del letter_map[s[left]]
+                left += 1    
+                
+        return result
