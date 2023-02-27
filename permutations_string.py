a9100f072edd70c27288b36c68e6b2edbccce198
abi26anamo <91598495+abi26anamo@users.noreply.github.com>
2023-02-27 14:22:13 +0300

    Update permutations_string.py

--- permutations_string.py
+++ permutations_string.py
@@ -1,13 +1,29 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         left = 0
-        right = len(s1)
-        
+        count_s1 = Counter(s1)
+        permut = {}
         contains = False
-        while right <=len(s2):
-            if sorted(s2[left:right]) ==sorted(s1):
-                contains = True
-                break
-            left+=1
-            right+=1
+        
+        for right in range(len(s2)):
+            if s2[right] in count_s1:
+                if s2[right] in permut:
+                     permut[s2[right]]+=1
+                else:
+                    permut[s2[right]]=1
+            else:
+                left = right+1
+                permut = {}
+                
+            if right-left+1==len(s1):
+                if permut == count_s1:
+                    contains =True
+                    break
+                    
+                #move window
+                permut[s2[left]] -= 1
+                if permut[s2[left]] == 0:
+                    del permut[s2[left]]
+                left += 1    
+                
         return contains
