5277ed2c742b2bb8250c26a41042a4d0c4acc951
abi26anamo <91598495+abi26anamo@users.noreply.github.com>
2023-02-28 15:38:25 +0300

    Update sub_array_sums_divisible_by_K.py

--- sub_array_sums_divisible_by_K.py
+++ sub_array_sums_divisible_by_K.py
@@ -1,12 +1,20 @@
 class Solution:
     def subarraysDivByK(self, nums: List[int], k: int) -> int:
-        dic =defaultdict(int)
-        subsum =0
+        n = len(nums)
+        prefix = [0 for _ in range(n+1)]
+        
+        #build the prefix sum
+        for i in range(1,n+1):
+            prefix[i] = prefix[i-1]+nums[i-1]
+        print(prefix)
         count =0
-        dic[0]=1
-        for i in range(len(nums)):
-            subsum+=nums[i]
-            rem = subsum%k
-            count+=dic[rem]
-            dic[rem]+=1  
+        hash_map =defaultdict(int)
+        
+        for right in range(len(prefix)):
+            if prefix[right]%k in hash_map:
+                count+=hash_map[prefix[right]%k]
+                hash_map[prefix[right]%k]+=1
+            else:
+                hash_map[prefix[right]%k]+=1
+        
         return count
