2f0c3f5eb4417be11226856fea2bc16f84a6fc9f
abi26anamo <91598495+abi26anamo@users.noreply.github.com>
2023-02-28 09:40:35 +0300

    Update product_subarray_except_self.py

--- product_subarray_except_self.py
+++ product_subarray_except_self.py
@@ -1,12 +1,23 @@
+# Time complexity is O(N)
+# space complexity is O(N)
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        out =[1]*len(nums)
-        prefix =1
-        for i in range(len(nums)):
-            out[i]=prefix
-            prefix*=nums[i]
-        postfix =1
-        for i in range(len(nums)-1,-1,-1):
-            out[i]*=postfix
-            postfix *=nums[i]
-        return out
+        n = len(nums)
+        left_product = [1]*n
+        right_product = [1]*n
+        
+        if n==1:
+            return []
+        
+        for i in range(1,n):
+            left_product[i]=left_product[i-1]*nums[i-1]
+            
+        for i in range(n-2,-1,-1):
+            right_product[i] = right_product[i+1]*nums[i+1]
+            
+        result = []
+        for i in range(n):
+            result.append(left_product[i]*right_product[i])
+            
+        return result
+            
