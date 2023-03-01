12ddd3a250ba83abb3e2af1bfb3b3f48cb85a4c3
abi26anamo <91598495+abi26anamo@users.noreply.github.com>
2023-03-01 22:09:30 +0300

    Update array_manipulation.py

--- array_manipulation.py
+++ array_manipulation.py
@@ -21,10 +21,11 @@ def arrayManipulation(n, queries):
         prefix_sum[query[0]-1]+=query[2]
         prefix_sum[query[1]]-=query[2]
         
+    running_sum = prefix_sum[0]
     max_val = prefix_sum[0]
     for i in range(1,len(prefix_sum)):
-        prefix_sum[i]+=prefix_sum[i-1]
-        max_val = max(max_val,prefix_sum[i])
+        running_sum+=prefix_sum[i]
+        max_val = max(max_val,running_sum)
     return max_val
     
 if __name__ == '__main__':
@@ -46,3 +47,4 @@ if __name__ == '__main__':
     fptr.write(str(result) + '\n')
 
     fptr.close()
+
