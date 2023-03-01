f82e30444871bc4380789a39cbc807e7dc47866a
abi26anamo <91598495+abi26anamo@users.noreply.github.com>
2023-03-01 17:43:06 +0300

    Update remove_duplicate_letters.py

--- remove_duplicate_letters.py
+++ remove_duplicate_letters.py
@@ -1,10 +1,20 @@
 class Solution:
     def removeDuplicateLetters(self, s: str) -> str:
-        d ={c:i for i,c in enumerate(s)}
-        res =[]
-        for i,c in enumerate(s):
-            if c not in res:
-                while res and d[res[-1]]>=i and c <res[-1]:
-                    res.pop()
-                res.append(c)
-        return "".join(res)
+        n = len(s)
+        stack = []
+        hash_set = set()
+        freq = Counter(s)
+        
+        for i in range(n):
+            
+            while stack and ord(stack[-1])>ord(s[i]) and freq[stack[-1]]>0 and s[i] not in hash_set:
+                hash_set.remove(stack[-1])
+                stack.pop()
+        
+            if s[i] not in hash_set:  
+                stack.append(s[i])
+                hash_set.add(s[i])
+                freq[s[i]]-=1
+            else:
+                freq[s[i]]-=1
+        return "".join(stack)
