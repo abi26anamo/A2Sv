6ba46a8f96c23d7bcccfb1850843578f3b48625f
abi26anamo <91598495+abi26anamo@users.noreply.github.com>
2023-03-04 12:01:45 +0300

    Update count_of_nice_subarray.py

--- count_of_nice_subarray.py
+++ count_of_nice_subarray.py
@@ -1,14 +1,25 @@
 class Solution:
     def numberOfSubarrays(self, nums: List[int], k: int) -> int:
-        count =0
-        prefix = [0]*len(nums)
-        odd =0
-        for i in range(len(nums)):
-            prefix[odd]+=1
-            
-            if nums[i]%2:
-                odd+=1
-            if odd >=k:
-                count+=prefix[odd-k]
+        n = len(nums)
+        arr = [1 if nums[i]%2!=0 else 0 for i in range(n)]
+
+        prefix_sum = 0
+        hash_map = {0:1}
+        count = 0
+        print(arr)
+ 
+        for right in range(n):
+            prefix_sum+=arr[right]
+
+            if prefix_sum-k in hash_map:
+                count+= hash_map[prefix_sum-k]
+                if prefix_sum in hash_map:
+                      hash_map[prefix_sum]+=1
+                else:
+                    hash_map[prefix_sum]=1
+ 
+            elif prefix_sum in hash_map:
+                hash_map[prefix_sum]+=1
+            else:
+                hash_map[prefix_sum]=1
         return count
-            
