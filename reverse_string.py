ef29b36be4a39e418021809205730ecc159a6e62
abi26anamo <91598495+abi26anamo@users.noreply.github.com>
2023-03-10 16:37:43 +0300

    Update reverse_string.py

--- reverse_string.py
+++ reverse_string.py
@@ -3,10 +3,10 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        left = 0
-        right = len(s)-1
-        while left < right:
-            s[left],s[right] = s[right],s[left]
-            left+=1
-            right-=1
+        def helper(start,end,st):
+            if start>=end:
+                return 
+            s[start],s[end] = s[end],s[start]
+            helper(start+1,end-1,st)
+        helper(0,len(s)-1,s)
         return s
