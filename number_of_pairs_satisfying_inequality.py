12cc28e879cb08d7968dad501584ef59eee395dd
abi26anamo <91598495+abi26anamo@users.noreply.github.com>
2023-03-28 21:42:17 +0300

    Create number_of_pairs_satisfying_inequality.py

---
+++ number_of_pairs_satisfying_inequality.py
@@ -0,0 +1,36 @@
+class Solution:
+   def numberOfPairs(self, nums1: List[int], nums2: List[int], diff: int) -> int:
+       self.count =0
+       nums = [nums1[i]-nums2[i] for i in range(len(nums1))]
+
+       def merge(merged,l_arr,r_arr):
+           i =0
+           j =0
+           while i < len(l_arr) and j < len(r_arr):
+               if l_arr[i]<=r_arr[j]:
+                   merged.append(l_arr[i])
+                   i+=1
+               else:
+                   merged.append(r_arr[j])
+                   j+=1
+           while i < len(l_arr):
+               merged.append(l_arr[i])
+               i+=1
+           while j < len(r_arr):
+               merged.append(r_arr[j])
+               j+=1
+           return merged
+
+       def mergesort(arr):
+           if len(arr)==1:
+              return arr
+           mid = len(arr)//2
+           left = mergesort(arr[:mid])
+           right = mergesort(arr[mid:])
+           
+           for i in left:
+               val = bisect_left(right,i-diff)               
+               self.count+=(len(right)-val)
+           return merge([],left,right)
+       mergesort(nums)
+       return self.count
