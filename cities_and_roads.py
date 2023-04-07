2c317516b408fcdb28aa426c5db4cb83771300c2
abi26anamo <91598495+abi26anamo@users.noreply.github.com>
2023-04-07 22:14:44 +0300

    Update cities_and_roads.py

--- cities_and_roads.py
+++ cities_and_roads.py
@@ -1,11 +1,11 @@
 n = int(input())
-edges = set()
+edges = 0
 graph = []
 for _ in range(n):
     row = [int(i)  for i in input().split()]
     graph.append(row)
 for i in range(n):
     for j in range(n):
-        if graph[i][j]==1 and (i,j) not in edges and  (j,i) not in edges:
-            edges.add((i,j))
-print(len(edges))
+        if graph[i][j]==1:
+            edges+=1
+print(edges//2)
