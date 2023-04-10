96ed1dddd3730869096e076f29bc7dcb6311409e
abi26anamo <91598495+abi26anamo@users.noreply.github.com>
2023-04-10 22:01:55 +0300

    Update 695-max-area-of-island.py

--- 695-max-area-of-island/695-max-area-of-island.py
+++ 695-max-area-of-island/695-max-area-of-island.py
@@ -1,16 +1,31 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        lenRow =len(grid)
-        lenCol = len(grid[0])
-        visit = set()
+        n = len(grid)
+        m = len(grid[0])
+        graph = defaultdict(list)
+        visited  = set()
+    
+        def inbound(r,c):
+            return 0<=r<len(grid) and 0<=c<len(grid[0])
+     
         def dfs(row,col):
-            if row<0 or row==lenRow or col<0 or col ==lenCol or grid[row][col]==0 or (row,col) in visit:
-                return 0
-            visit.add((row,col))
-            return 1+dfs(row+1,col)+dfs(row-1,col)+dfs(row,col+1)+dfs(row,col-1)
-        ans = 0
-        for row in range(lenRow):
-            for col in range(lenCol):
-                ans = max(ans,dfs(row,col))
-        return ans
+            directions = [(-1,0),(0,-1),(0,1),(1,0)]
+            visited.add((row,col))
+            for r,c in directions:
+                new_row = row+r
+                new_col = col+c
+                if inbound(new_row,new_col) and (new_row,new_col) not in visited and grid[new_row][new_col]==1:
+                    dfs(new_row,new_col)
+
+        max_area =0
+        all_visited = set()
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j]==1 and (i,j) not in all_visited:
+                    dfs(i,j)
+                max_area  = max(max_area,len(visited))
+                for v in visited:
+                    all_visited.add(v)
+                visited = set()
+        return max_area
                 
