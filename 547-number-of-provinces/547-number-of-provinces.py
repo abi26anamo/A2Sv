b38d4f922247dcbd70f5ddff9dbf6cff46e2673f
abi26anamo <91598495+abi26anamo@users.noreply.github.com>
2023-04-10 10:36:58 +0300

    Update 547-number-of-provinces.py

--- 547-number-of-provinces/547-number-of-provinces.py
+++ 547-number-of-provinces/547-number-of-provinces.py
@@ -1,15 +1,34 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        seen = set()
+        n = len(isConnected)
+        graph = defaultdict(set)
+        res = []
+        for i in range(n):
+            for j in range(n):
+                if isConnected[i][j]==1:
+                    graph[i+1].add(j+1)
+                    graph[j+1].add(i+1)
+
+        visited = set()
         def dfs(node):
-            for i,adj in enumerate(isConnected[node]):
-                if adj and i not in seen:
-                    seen.add(i)
-                    dfs(i)
-        ans = 0
-        for i in range(len(isConnected)):
-            if i not in seen:
-                dfs(i)
-                ans+=1
-        return ans
+            visited.add(node)
+            for neighbor in graph[node]:
+                if neighbor not in visited:
+                    dfs(neighbor)
+        
+        for vertex in graph:
+            if vertex not in visited:
+                provinces = set()
+                dfs(vertex)
+                for v in visited:
+                    provinces.add(v)
+            if provinces not in res:
+                   res.append(provinces)
+            visited = set()
+        return len(res)
+            
+
+
+
+        
        
