d3f20580af521e1c130ed58bd7f95da52e6b407a
abi26anamo <91598495+abi26anamo@users.noreply.github.com>
2023-10-03 15:43:03 +0300

    Update last_stone_weight_II.py

--- last_stone_weight_II.py
+++ last_stone_weight_II.py
@@ -1,11 +1,18 @@
 class Solution:
     def lastStoneWeightII(self, stones: List[int]) -> int:
-        if not stones:
-           return 0
-        total = sum(stones)
-        target = total // 2
-        dp = [0] * (target + 1)
-        for stone in stones:
-            for i in range(target, stone - 1, -1):
-                dp[i] = max(dp[i], dp[i - stone] + stone)
-        return total - 2 * dp[target]
+        dp = {}
+        n = len(stones)
+        target = ceil(sum(stones)/2)
+
+        def dfs(i,total):
+            if total>=target or i == len(stones):
+                return abs(total- (sum(stones)-total))
+            
+            if (i,total) in dp:
+                return dp[(i,total)]
+            
+            dp[(i,total)] = min(dfs(i+1,total),dfs(i+1,total+stones[i]) )
+            
+            return dp[(i,total)]
+
+        return dfs(0,0)
